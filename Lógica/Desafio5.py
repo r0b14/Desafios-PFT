c0f1b0bcbd2059843f0baeafb69fe859a1087fc8
r0b14-ubuntu <rtds@cin.ufpe.br>
2025-05-14 10:52:53 -0300

    add challenge 5

---
+++ Lógica/Desafio5.py
@@ -0,0 +1,13 @@
+# Usando diversos tipos de dados - Calculadora
+
+# Calculadora
+
+number1 = int(input("Digite o 1 numero: "))
+number2 = int(input("Digite o 2 numero: "))
+
+sum = number1 + number2 #soma
+subtraction = number1 - number2 #subtração
+division = number1 / number2 #divisão
+multiplication = number1 * number2 #multiplicação
+
+print(f"Soma= {sum} \nSubtração= {subtraction} \nDivisão= {division} \nMultiplicação= {multiplication}")
