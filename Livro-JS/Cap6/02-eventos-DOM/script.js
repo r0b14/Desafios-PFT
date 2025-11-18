043b04241d171f8c3959024dcb059496c7ce1074
rtds-dell <rtds@cin.ufpe.br>
2025-11-18 01:07:52 -0300

    add new challeng focus on book

---
+++ Livro-JS/Cap6/02-eventos-DOM/script.js
@@ -0,0 +1,7 @@
+document.getElementById('myButton').addEventListener('click', () => {
+    alert("Botão clicado!")
+})
+
+document.getElementById('myParagraph').addEventListener('mouseover', () => {
+    console.log("O mouse está sobre o texto.")
+})
---
+++ Livro-JS/Cap6/exercicio/script.js
@@ -0,0 +1,28 @@
+let mainDoc = document.getElementById("main");
+console.log(mainDoc);
+
+let titulo = document.getElementById("titulo");
+let tituloOriginal = titulo.textContent;
+
+console.log(titulo);
+// Evento para QUANDO O MOUSE ENTRA
+titulo.addEventListener('mouseover', () => {
+    titulo.textContent = "Novo Titulo - Via JS";
+});
+// Evento para QUANDO O MOUSE SAI
+titulo.addEventListener('mouseout', () => {
+    titulo.textContent = tituloOriginal;
+});
+
+// 'paragrafo' aqui é uma HTMLCollection (uma lista)
+let paragrafo = document.getElementsByClassName("paragrafo");
+console.log(paragrafo);
+
+// Verificamos se a lista não está vazia (boa prática)
+if (paragrafo.length > 0) {
+    // Acessamos o PRIMEIRO item (índice 0) e mudamos o HTML dele
+    paragrafo[0].innerHTML = '<p>Novo Paragrafo JS</p>';
+}
+
+let button = document.getElementsByTagName("button");
+console.log(button)
